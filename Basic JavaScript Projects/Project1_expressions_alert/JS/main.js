f14d58988e1675e3f3a01dd5df5627003a124fc5
aspringbreeze <99435565+aspringbreeze@users.noreply.github.com>
2024-05-24 11:58:28 -0700

    Added simple JavaScript code
    Write simple code, including defining variable, concatenating and writing expressions.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+window.alert("Hello, World!");
+document.write("Hello, World!");
+var A = "This is a string."; //Defining a variable and giving it a string value
+document.write(A);
+var B = "Tom didn\'t eat vege and said, \"I'll choose steak.\"";
+document.write(B);
+document.write(A+B);
+document.write(18*16);
